e8cb70bac598fa2f2f8d2d476e9f27aaf73947f8
Jaedw8 <jaedw8@morgan.edu>
2018-02-11 18:40:55 -0500

    updated

---
+++ flix_Project1/Views/PosterCell.swift
@@ -0,0 +1,17 @@
+//
+//  PosterCell.swift
+//  flix_Project1
+//
+//  Created by Jasmine Edwards on 2/11/18.
+//  Copyright © 2018 Jasmine Edwards. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell
+{
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
